b157da80250f13159dd9e211c5197435d6e7eced
Sheshank Shankar <sheshank@outlook.com>
2017-09-30 16:44:48 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ function randomNumber (max, min) {
 }
 
 function onClick() {
+ 
+ document.getElementById("output").style.cssText = "font-size:1px;transition: all 0.5s ease;";
    var max = parseInt(document.getElementById("maxInput").value);
    var min = parseInt(document.getElementById("minInput").value);
    var rand = randomNumber(max,min);
