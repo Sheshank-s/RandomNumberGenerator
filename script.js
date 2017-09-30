3aed083e54a3db3dfc38c8cff92ba3480c545c72
Sheshank Shankar <sheshank@outlook.com>
2017-09-30 15:35:02 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,9 @@
 function randomNumber (max, min) {
  return (Math.floor(Math.random()*(max-min))+1)+min;   
 }
+
+function onClick() {
+   var max = document.getElementById("maxInput").value;
+   var min = document.getElementById("min").value;
+   alert(randomNumber(max,min));
+}
